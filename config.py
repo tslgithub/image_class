dbaa94f73a6d7b9498a410f4cf4552298eae30e7
xxy@xxy-gpu <mymailwith163@163.com>
2019-01-16 11:19:27 +0800

    update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ author:tslgithub
 email:mymailwith163@163.com
 time:2018-12-12
 msg: You can choose the following model to train your image, and just switch in config.py:
-    VGG16,VGG19,InceptionV3,Xception,MobileNet,AlexNet,LeNet,ZF_Net,esNet18,ResNet34,ResNet50,ResNet_101,ResNet_152
+    VGG16,VGG19,InceptionV3,Xception,MobileNet,AlexNet,LeNet,ZF_Net,ResNet18,ResNet34,ResNet50,ResNet_101,ResNet_152
 """
 
 """
@@ -35,23 +35,31 @@ msg: You can choose the following model to train your image, and just switch in 
 """
 
 class DefaultConfig():
+    """
+        # You can choose the following model:
+        VGG16,VGG19,InceptionV3,Xception,MobileNet,AlexNet,LeNet,ZF_Net
+        ResNet18,ResNet34,ResNet50,ResNet_101,ResNet_152
+        """
+    model_name = 'ResNet50'
+
     train_data_path = './dataset/train/'
     test_data_path = './dataset/test/'
-    channles = 1
     checkpoints = './checkpoints/'
-    normal_size = 64
+    channles = 1
+
+    if model_name == 'InceptionV3':
+        normal_size = 75
+    elif model_name == 'Xception':
+        normal_size = 71
+    else:
+        normal_size = 64
+
     epochs = 100
-    batch_size = 2
+    batch_size = 32
     data_augmentation = True
-    classes = 2
+    classes = 3
     lr = 0.0001
     default_optimizers = False
-    data_augmentation = True
-    """
-    # You can choose the following model:
-    VGG16,VGG19,InceptionV3,Xception,MobileNet,AlexNet,LeNet,ZF_Net
-    ResNet18,ResNet34,ResNet50,ResNet_101,ResNet_152
-    """
-    model_name = 'VGG16'
+
 
 config = DefaultConfig()
