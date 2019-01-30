769b00a7c1856cce74f06475077e8c04697b14ec
田松禄 <mymailwith163@163.com>
2019-01-30 17:22:38 +0800

    update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ author:tslgithub
 email:mymailwith163@163.com
 time:2018-12-12
 msg: You can choose the following model to train your image, and just switch in config.py:
-    VGG16,VGG19,InceptionV3,Xception,MobileNet,AlexNet,LeNet,ZF_Net,ResNet18,ResNet34,ResNet50,ResNet_101,ResNet_152
+    VGG16,VGG19,InceptionV3,Xception,MobileNet,AlexNet,LeNet,ZFNet,ResNet18,ResNet34,ResNet50,ResNet101,ResNet152
 """
 
 """
