b38f3dda02f2b6e43ecea601c89fa68edcc713c7
tsl-777 <mymailwith163@163.com>
2019-07-05 18:05:49 +0800

    Build_model.py

--- Build_model.py
+++ Build_model.py
@@ -33,22 +33,28 @@ class Build_model(object):
         self.cut = config.cut
 
     def model_confirm(self,choosed_model):
-
-
         if choosed_model == 'VGG16':
-            model = keras.applications.VGG16(include_top=True,
-                                                   weights=None,
-                                                   input_tensor=None,
-                                                   input_shape=(self.normal_size,self.normal_size,self.channles),
-                                                   pooling='max',
-                                                   classes=self.classes)
+            model = MODEL(self.config).VGG16()
         elif choosed_model == 'VGG19':
-            model = keras.applications.VGG19(include_top=True,
-                                                   weights=None,
-                                                   input_tensor=None,
-                                                   input_shape=(self.normal_size,self.normal_size,self.channles),
-                                                   pooling='max',
-                                                   classes=self.classes)
+            model = MODEL(self.config).VGG19()
+        elif choosed_model == 'AlexNet':
+            model = MODEL(self.config).AlexNet()
+        elif choosed_model == 'LeNet':
+            model = MODEL(self.config).LeNet()
+        elif choosed_model == 'ZF_Net':
+            model = MODEL(self.config).ZF_Net()
+        elif choosed_model == 'ResNet18':
+            model = ResnetBuilder().build_resnet18(self.config)
+        elif choosed_model == 'ResNet34':
+            model = ResnetBuilder().build_resnet34(self.config)
+        elif choosed_model == 'ResNet101':
+            model = ResnetBuilder().build_resnet101(self.config)
+        elif choosed_model == 'ResNet152':
+            model = ResnetBuilder().build_resnet152(self.config)
+        elif choosed_model =='mnist_net':
+            model = MODEL(self.config).mnist_net()
+        elif choosed_model == 'TSL16':
+            model = MODEL(self.config).TSL16()
 
         elif choosed_model == 'ResNet50':
             model = keras.applications.ResNet50(include_top=True,
@@ -79,40 +85,21 @@ class Build_model(object):
                                                  input_shape=(self.normal_size,self.normal_size,self.channles),
                                                  pooling='max',
                                                  classes=self.classes)
-        elif choosed_model == 'DenseNet':
-            depth = 40
-            nb_dense_block = 3
-            growth_rate = 12
-            nb_filter = 12
-            bottleneck = False
-            reduction = 0.0
-            dropout_rate = 0.0
-
-            img_dim = (self.channles, self.normal_size) if K.image_dim_ordering() == "th" else (
-                self.normal_size, self.normal_size, self.channles)
-
-            model = densenet.DenseNet(img_dim, classes=self.classes, depth=depth, nb_dense_block=nb_dense_block,
-                                      growth_rate=growth_rate, nb_filter=nb_filter, dropout_rate=dropout_rate,
-                                      bottleneck=bottleneck, reduction=reduction, weights=None)
-
-        elif choosed_model == 'AlexNet':
-            model = MODEL(self.config).AlexNet()
-        elif choosed_model == 'LeNet':
-            model = MODEL(self.config).LeNet()
-        elif choosed_model == 'ZF_Net':
-            model = MODEL(self.config).ZF_Net()
-        elif choosed_model == 'ResNet18':
-            model = ResnetBuilder().build_resnet18(self.config)
-        elif choosed_model == 'ResNet34':
-            model = ResnetBuilder().build_resnet34(self.config)
-        elif choosed_model == 'ResNet101':
-            model = ResnetBuilder().build_resnet101(self.config)
-        elif choosed_model == 'ResNet152':
-            model = ResnetBuilder().build_resnet152(self.config)
-        elif choosed_model =='mnist_net':
-            model = MODEL(self.config).mnist_net()
-        elif choosed_model == 'VGG16_TSL':
-            model = MODEL(self.config).VGG16_TSL()
+        # elif choosed_model == 'DenseNet':
+        #     depth = 40
+        #     nb_dense_block = 3
+        #     growth_rate = 12
+        #     nb_filter = 12
+        #     bottleneck = False
+        #     reduction = 0.0
+        #     dropout_rate = 0.0
+        #
+        #     img_dim = (self.channles, self.normal_size) if K.image_dim_ordering() == "th" else (
+        #         self.normal_size, self.normal_size, self.channles)
+        #
+        #     model = densenet.DenseNet(img_dim, classes=self.classes, depth=depth, nb_dense_block=nb_dense_block,
+        #                               growth_rate=growth_rate, nb_filter=nb_filter, dropout_rate=dropout_rate,
+        #                               bottleneck=bottleneck, reduction=reduction, weights=None)
 
         return model
 
