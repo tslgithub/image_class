ca502a449e04658d98b9210304535be53749b097
tsl-777 <mymailwith163@163.com>
2019-07-29 20:43:20 +0800

    update predict.py

--- predict.py
+++ predict.py
@@ -27,7 +27,13 @@ class PREDICT(Build_model):
     def __init__(self,config):
         Build_model.__init__(self,config)
         self.test_data_path = os.path.join(config.test_data_path,sys.argv[1])
-        # self.test_data_path = os.path.join(config.test_data_path,"1")
+        # self.test_data_path = os.path.join(config.test_data_path,"12667905159247769")
+
+    def classes_id(self):
+        with open('train_class_idx.txt','r') as f:
+            lines = f.readlines()
+            lines = [line.rstrip() for line in lines]
+        return lines
 
     def Predict(self):
         model = Build_model(self.config).build_model()
@@ -37,23 +43,24 @@ class PREDICT(Build_model):
             print('weights is not exist')
         model.load_weights(os.path.join(os.path.join(self.checkpoints,self.model_name),self.model_name+'.h5'))
         data_list = list(map(lambda x: cv2.resize(cv2.imread(os.path.join(self.test_data_path,x),int(self.channles/3)),
-                                                  (self.normal_size,self.normal_size)),os.listdir(self.test_data_path)     ))
+                                                  (self.normal_size,self.normal_size)),os.listdir(self.test_data_path)))
         i,j,tmp = 0,0,[]
         for img in data_list:
             img = np.array([img_to_array(img)],dtype='float')/255.0
             pred = model.predict(img).tolist()[0]
-            label = pred.index(max(pred))
+            # label = pred.index(max(pred))
+            label = self.classes_id()[pred.index(max(pred))]
             confidence = max(pred)
             print('predict label     is: ',label)
             print('predict confidect is: ',confidence)
+            # if label != '12667905159247769':
             if label != sys.argv[1]:
                 print('wrong label :_____________________________________________wrong ', label)
                 i+=1
                 tmp.append(label)
             else:
                 j+=1
-        print('error number: ', i, '\ntotal: ', i + j, '\naccuacy is: ', str((1.0 - i / (len(data_list)))*100)+' %'  )
-        print('error: ', ','.join(list(map(lambda x: str(x), tmp))))
+        print('\naccuacy is:%.2f'%((1.0 - i / (len(data_list)))*100),"%")
         print('Done')
 
 def main():
