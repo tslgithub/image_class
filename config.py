61b6bad037f750bc037a6b5b043d429130582a5d
tsl-777 <mymailwith163@163.com>
2019-09-07 18:25:01 +0800

    update config.py

--- config.py
+++ config.py
@@ -10,10 +10,15 @@ msg: You can choose the following model to train your image, and just choose in 
 
 import sys
 class DefaultConfig():
-    # model_name = 'ResNet34'
-    model_name = sys.argv[1]
-    train_data_path = '/media/tsl/DataBackup/TrafficSignalDataset/training/'
-    test_data_path = '/media/tsl/DataBackup/TrafficSignalDataset/testing/'
+
+    try:
+        model_name = sys.argv[1]
+    except:
+        print("use default model VGG16, see config.py")
+        model_name = "VGG16"
+
+    train_data_path = './dataset/train/'
+    test_data_path = './dataset/test/'
     checkpoints = './checkpoints/'
 
     if model_name == 'InceptionV3':
@@ -23,17 +28,16 @@ class DefaultConfig():
     else:
         normal_size = 64
     # normal_size = 48
-    epochs = 30
-    batch_size = 8
-    data_augmentation = False
-    classes = 49
+    epochs = 1
+    batch_size = 2
+    classNumber = 2 # see dataset/tri
     channles = 3 # or 3 or 1
-
     lr = 0.00001
-    monitor = 'val_loss'
-    lr_reduce_patience = 5 #需要降低学习率的训练步长
-    early_stop_patience = 10  #提前终止训练的步长
+    lr_reduce_patience = 5  # 需要降低学习率的训练步长
+    early_stop_patience = 11  # 提前终止训练的步长
 
+    data_augmentation = False
+    monitor = 'val_loss'
     cut = False
     rat = 0.1 #if cut,img[slice(h*self.rat,h-h*self.rat),slice(w*self.rat,w-w*self.rat)]
 
