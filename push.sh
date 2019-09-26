cfb6715e50b5e964143a67026b344e68082b3706
tsl-777 <mymailwith163@163.com>
2019-09-26 10:25:27 +0800

    update push.sh

--- push.sh
+++ push.sh
@@ -1,6 +1,13 @@
-echo "push origin master"
-echo "input file name"
-read FILE
-git add $FILE
-git commit -m "update $FILE"
+#!/usr/bin/env bash
+#echo "push origin master"
+#echo "input file name"
+#read FILE
+#git add $FILE
+#git commit -m "update $FILE"
+#git push origin master
+
+echo "push update file: $1"
+git add $1
+git commit -m "update $1"
 git push origin master
+
