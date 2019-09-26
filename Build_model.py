2eb4b7f4ea698064f9193a2404920a24a7617d93
tsl-777 <mymailwith163@163.com>
2019-09-26 10:23:42 +0800

    update Build_model.py

--- Build_model.py
+++ Build_model.py
@@ -12,7 +12,7 @@ from MODEL import MODEL,ResnetBuilder
 import sys
 sys.setrecursionlimit(10000)
 
-from keras import backend as K
+# from keras import backend as K
 # import densenet   #取消densenet模型
 
 class Build_model(object):
@@ -55,21 +55,20 @@ class Build_model(object):
             model = MODEL(self.config).mnist_net()
         elif choosed_model == 'TSL16':
             model = MODEL(self.config).TSL16()
-
         elif choosed_model == 'ResNet50':
             model = keras.applications.ResNet50(include_top=True,
                                                    weights=None,
                                                    input_tensor=None,
                                                    input_shape=(self.normal_size,self.normal_size,self.channles),
                                                    pooling='max',
-                                                   classNumber=self.classNumber)
+                                                   classes=self.classNumber)
         elif choosed_model == 'InceptionV3':
             model = keras.applications.InceptionV3(include_top=True,
                                                    weights=None,
                                                    input_tensor=None,
                                                    input_shape=(self.normal_size,self.normal_size,self.channles),
                                                    pooling='max',
-                                                   classNumber=self.classNumber)
+                                                   classes=self.classNumber)
 
         elif choosed_model == 'Xception':
             model = keras.applications.Xception(include_top=True,
@@ -77,14 +76,14 @@ class Build_model(object):
                                                 input_tensor=None,
                                                 input_shape=(self.normal_size,self.normal_size,self.channles),
                                                 pooling='max',
-                                                classNumber=self.classNumber)
+                                                classes=self.classNumber)
         elif choosed_model == 'MobileNet':
             model = keras.applications.MobileNet(include_top=True,
                                                  weights=None,
                                                  input_tensor=None,
                                                  input_shape=(self.normal_size,self.normal_size,self.channles),
                                                  pooling='max',
-                                                 classNumber=self.classNumber)
+                                                 classes=self.classNumber)
         # elif choosed_model == 'DenseNet':
         #     depth = 40
         #     nb_dense_block = 3
@@ -106,13 +105,9 @@ class Build_model(object):
     def model_compile(self,model):
         adam = keras.optimizers.Adam(lr=self.lr)
         model.compile(loss="categorical_crossentropy", optimizer=adam, metrics=["accuracy"])  # compile之后才会更新权重和模型
-
-
         return model
 
     def build_model(self):
-
         model = self.model_confirm(self.model_name)
         model = self.model_compile(model)
-
         return model
