9e19517b8e6431a2ac9ace7f8ae521234ec88041
tsl-777 <mymailwith163@163.com>
2020-03-13 16:32:48 +0800

    update config.py

--- config.py
+++ config.py
@@ -14,27 +14,29 @@ class DefaultConfig():
     try:
         model_name = sys.argv[1]
     except:
-        print("use default model LeNet, see config.py")
-        model_name = "LeNet"
+        print("use default model VGG16, see config.py")
+        model_name = "VGG16"
 
-    train_data_path = './dataset/train/'
-    test_data_path = './dataset/test/'
+    train_data_path = 'dataset/train'
+    test_data_path = 'dataset/test'
     checkpoints = './checkpoints/'
 
-    if model_name == 'InceptionV3':
-        normal_size = 75#minSize
-    elif model_name == 'Xception':
-        normal_size = 71#minSize
-    else:
-        normal_size = 64
-    # normal_size = 48
+    # if model_name == 'InceptionV3':
+    #     normal_size = 75#minSize
+    # elif model_name == 'Xception':
+    #     normal_size = 71#minSize
+    # else:
+    #     normal_size = 224
+
+    normal_size = 224
     epochs = 1
     batch_size = 2
-    classNumber = 2 # see dataset/tri
+    classNumber = 2 # see dataset
     channles = 3 # or 3 or 1
-    lr = 0.00001
+    lr = 0.001
+
     lr_reduce_patience = 5  # 需要降低学习率的训练步长
-    early_stop_patience = 11  # 提前终止训练的步长
+    early_stop_patience = 10  # 提前终止训练的步长
 
     data_augmentation = False
     monitor = 'val_loss'
